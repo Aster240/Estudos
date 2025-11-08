08eedb1030c4cf8562b40876e1c6d262e592f6c2
Rafael Sousa Marques <rafael.marques.sousa.9@gmail.com>
2025-11-08 18:48:17 -0300

    nem sei mais

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from calls.call_viewers import Buscar_boletim_por_aluno, Buscar_todos_boletins
+from call.praticando import *
 
 def main():
-    print('inicialização do menu:')
+    print('inicialização do menu:(')
+    
+    test();
+
 
 if __name__ == "__main__":
     main()
